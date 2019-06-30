97940976ad8c2ea6b6ac1c244917e34ac1b420fd
bhoomi <bhoomi@LAPTOP-7RM6SAN7>
2019-06-30 08:09:59 +0530

    updated 1

---
+++ src/test/java/com/krishna/start.java
@@ -0,0 +1,44 @@
+package com.krishna;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+public class start {
+@Test
+@Parameters({"url","email"})
+public void starr(String url,String email){
+	System.out.println(url);
+	System.out.println("Hello1");
+	System.out.println("QAq"+System.getProperty("environment"));
+	Properties properties = new Properties();
+	System.out.println(properties.getProperty("${project.build.sourceEncoding}"));
+	/*Properties properties = new Properties();
+	
+	System.out.println(properties.getProperty("environment"));*/
+	/*
+	System.getProperty("webdriver.chrome.driver","C:\\Users\\bhoomi\\workspace\\test\\chromedriver.exe");
+	DesiredCapabilities capabilities = new DesiredCapabilities();
+	capabilities.setCapability("something", true);
+	ChromeOptions options = new ChromeOptions();
+	options.addArguments("disable-infobars");
+	
+	//ChromeOptions options = new ChromeOptions();
+	WebDriver dr=new ChromeDriver(capabilities);
+	dr.get("https://opensource-demo.orangehrmlive.com/");
+	dr.manage().window().maximize();
+	dr.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	String title=dr.getTitle();
+	System.out.println("The title is "+title);
+	dr.close();
+	*/
+}
+}
